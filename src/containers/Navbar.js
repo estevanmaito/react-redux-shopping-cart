7a957c3581e970518b2ab8ab6c9c35132c34f073
Estevan Maito <ejmaito@gmail.com>
2019-04-02 15:08:32 -0300

    cleanup navbar before creating cart component

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,25 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { connect } from "react-redux";
 
-export const Navbar = props => {
-  const { cart } = props;
-  return (
-    <Wrapper>
-      <p>Cart itens: {cart.map(product => product.name)}</p>
-    </Wrapper>
-  );
-};
+export const Navbar = () => <Wrapper>Cart</Wrapper>;
 
 const Wrapper = styled.nav`
   padding: 0.5rem;
   background-color: red;
 `;
 
-const mapStateToProps = state => {
-  return {
-    cart: state.cart
-  };
-};
-
-export default connect(mapStateToProps)(Navbar);
+export default Navbar;
