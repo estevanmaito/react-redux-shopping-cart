2a0dc71e01a724fa10424684917efdaa08f91871
Estevan Maito <ejmaito@gmail.com>
2019-04-03 10:56:28 -0300

    add item ids to initial state

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,30 +3,35 @@ import { CART_ADD_PRODUCT, CART_REMOVE_PRODUCT, CART_RESET } from "./actions";
 export const initialState = {
   products: [
     {
+      id: 1,
       imgUrl:
         "https://guesseu.scene7.com/is/image/GuessEU/M63H24W7JF0-L302-ALTGHOST?wid=1500&fmt=jpeg&qlt=80&op_sharpen=0&op_usm=1.0,1.0,5,0&iccEmbed=0",
       name: "CHECK PRINT SHIRT",
       price: 110
     },
     {
+      id: 2,
       imgUrl:
         "https://guesseu.scene7.com/is/image/GuessEU/FLGLO4FAL12-BEIBR?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
       name: "GLORIA HIGH LOGO SNEAKER",
       price: 91
     },
     {
+      id: 3,
       imgUrl:
         "https://guesseu.scene7.com/is/image/GuessEU/HWVG6216060-TAN?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
       name: "CATE RIGID BAG",
       price: 94.5
     },
     {
+      id: 4,
       imgUrl:
         "http://guesseu.scene7.com/is/image/GuessEU/WC0001FMSWC-G5?wid=520&fmt=jpeg&qlt=80&op_sharpen=0&op_usm=1.0,1.0,5,0&iccEmbed=0",
       name: "GUESS CONNECT WATCH",
       price: 438.9
     },
     {
+      id: 5,
       imgUrl:
         "https://guesseu.scene7.com/is/image/GuessEU/AW6308VIS03-SAP?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
       name: "'70s RETRO GLAM KEFIAH",
